5448e3356dac0e5dd2ad4608abee59947996cfcd
Denys Kibalenko <dennieking87@gmail.com>
2025-11-12 12:33:22 +0100

    Feat: add src/config/settings.py with get_settings which returns a settings class base on ENVIRONMENT env variable, can either be BaseAppSettings or TestingSettings

---
+++ src/config/settings.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+from pydantic_settings import BaseSettings
+
+
+class BaseAppSettings(BaseSettings):
+    BASE_DIR: Path(__file__).parent.parent
+    PATH_TO_DB: str = str(
+        BASE_DIR / "database" / "source" / "online_cinema.db"
+    )
+    PATH_TO_MOVIES_CSV: str = str(
+        BASE_DIR / "database" / "seed_data" / "imdb_movies.csv"
+    )
+
+
+class TestingSettings(BaseAppSettings):
+    PATH_TO_DB: str = ":memory:"
+
+
+def get_settings() -> BaseSettings:
+    """
+    Retrieve the application settings based on the environment.
+
+    This function checks the `ENVIRONMENT` environment variable to determine
+    which settings class to use. If `ENVIRONMENT` is set to `"testing"`, it
+    returns an instance of `TestingSettings`. Otherwise, it defaults to 
+    `BaseAppSettings`.
+
+    :return: An instance of the appropriate settings class.
+    :rtype: BaseSettings
+    """
+    environment = os.getenv("ENVIRONMENT", "developing")
+    if environment == "testing":
+        return TestingSettings()
+    return BaseAppSettings()
