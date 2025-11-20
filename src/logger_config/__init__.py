c4601b1b508983c8ff360e29ece84cf4854a7839
Denys Kibalenko <dennieking87@gmail.com>
2025-11-20 11:31:57 +0100

    Feat: setup project's logger
    * add 'setup_logging', 'get_logger' to src/logger_config/logger.py
    * add src/logger_config/config.json with logger setup

---
+++ src/logger_config/__init__.py
@@ -0,0 +1,1 @@
+from logger_config.logger import setup_logging, get_logger
---
+++ src/logger_config/logger.py
@@ -0,0 +1,32 @@
+from functools import lru_cache
+import json
+import logging.config
+from pathlib import Path
+
+
+def setup_logging() -> None:
+    """
+    Loads logging configuration from the external JSON file and 
+    applies it using dictConfig.
+    """
+
+    try:
+        config_file = Path("logger_config/config.json")
+        if not config_file.exists():
+            raise FileNotFoundError(f"Path {config_file} does not exist")
+
+        with open(config_file) as f_in:
+            config = json.load(f_in)
+
+        logging.config.dictConfig(config)
+    except Exception as e:
+        logging.basicConfig(level=logging.WARNING)
+        logging.warning(
+            f"Failed to load logging configuration from JSON. Error: {e}"
+        )
+
+
+def get_logger() -> logging.Logger:
+    """Returns tha main, configured 'app_logger' instance."""
+
+    return logging.getLogger("app_logger")
