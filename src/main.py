386b5322379bc060a8d741ead9c506eb885f5547
Denys Kibalenko <dennieking87@gmail.com>
2025-11-21 10:34:51 +0100

    Refactor: move project's logger object to src/main.py & remove lifespan functionality from the main app

--- src/main.py
+++ src/main.py
@@ -1,30 +1,20 @@
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
-from database import init_db, close_db
+from logger_config import get_logger, setup_logging
 
 
-def create_app() -> FastAPI:
-    @asynccontextmanager
-    async def lifespan(app: FastAPI):
-        await init_db()
-        yield
-        await close_db()
+setup_logging()
+log = get_logger()
 
-    app = FastAPI(
-        title="Online Cinema",
-        description=(
-            "A digital platform that allows users to select, watch, "
-            "and purchase access to movies and other video materials "
-            "via the internet."
-        ),
-        lifespan=lifespan
+
+app = FastAPI(
+    title="Online Cinema",
+    description=(
+        "A digital platform that allows users to select, watch, "
+        "and purchase access to movies and other video materials "
+        "via the internet."
     )
-
-    return app
-
-
-app = create_app()
+)
 
 
 @app.get("/users/")
