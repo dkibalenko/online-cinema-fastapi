cc9dcfa9380fbd428fe948d2d2158b637e468d4e
Denys Kibalenko <dennieking87@gmail.com>
2025-11-24 11:33:46 +0100

    Feat: implement 'Settings' class with PostgreSQL database settings in src/config/settings.py'

--- src/config/settings.py
+++ src/config/settings.py
@@ -16,6 +16,14 @@ class BaseAppSettings(BaseSettings):
     )
 
 
+class Settings(BaseAppSettings):
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER", "cinema_user")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "cinema_password")
+    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "cinema_host")
+    POSTGRES_DB_PORT: int = int(os.getenv("POSTGRES_DB_PORT", 5432))
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "cinema_db")
+
+
 class TestingSettings(BaseAppSettings):
     PATH_TO_DB: str = ":memory:"
 
@@ -27,12 +35,12 @@ def get_settings() -> BaseSettings:
     This function checks the `ENVIRONMENT` environment variable to determine
     which settings class to use. If `ENVIRONMENT` is set to `"testing"`, it
     returns an instance of `TestingSettings`. Otherwise, it defaults to 
-    `BaseAppSettings`.
+    `Settings`.
 
     :return: An instance of the appropriate settings class.
-    :rtype: BaseSettings
+    :rtype: Settings
     """
     environment = os.getenv("ENVIRONMENT", "developing")
     if environment == "testing":
         return TestingSettings()
-    return BaseAppSettings()
+    return Settings()
