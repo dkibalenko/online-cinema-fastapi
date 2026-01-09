d5d4de6bc5c9ec596c38a6d4409431330eb25dbe
Denys Kibalenko <dennieking87@gmail.com>
2026-01-09 17:16:01 +0100

    Feat: add commands/run_migrations.sh, commands/run.sh for running alembic to handle migrations on startup & run database population scripts from src/database/seeding/

--- commands/run.sh
+++ commands/run.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-echo "Running database saver script..."
-python -m database.populate_db
-echo "Database saver script completed."
-
 echo "Starting Uvicorn server..."
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload --reload-dir /usr/src/fastapi
