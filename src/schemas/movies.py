bf4aa47e694bc7b79bf6dcd515f0d67746a240e9
Denys Kibalenko <dennieking87@gmail.com>
2026-01-12 10:16:48 +0100

    Feat: add 'MovieDetailSchema', 'CertificationSchema', 'GenreSchema', 'StarSchema', 'DirectorSchema' to src/schemas/movies.py

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,13 +1,41 @@
 from decimal import Decimal
-from typing import Optional
+from typing import List, Optional
 import uuid
 
 from pydantic import BaseModel, Field
 
 
+class CertificationSchema(BaseModel):
+    id: int
+    name: str
+
+    model_config = {"from_attributes": True}
+
+
+class GenreSchema(BaseModel):
+    id: int
+    name: str
+
+    model_config = {"from_attributes": True}
+
+
+class StarSchema(BaseModel):
+    id: int
+    name: str
+
+    model_config = {"from_attributes": True}
+
+
+class DirectorSchema(BaseModel):
+    id: int
+    name: str
+
+    model_config = {"from_attributes": True}
+
+
 class MovieBaseSchema(BaseModel):
     name: str = Field(..., max_length=255)
-    year: int
+    year: int = Field(..., ge=1900, le=2026)
     time: int
     imdb: float = Field(..., ge=0, le=10)
     votes: int
@@ -25,5 +53,16 @@ class MovieListItemSchema(BaseModel):
     year: int
     imdb: float
     description: str
+
+    model_config = {"from_attributes": True}
+
+
+class MovieDetailSchema(MovieBaseSchema):
+    id: int
+    uu_id: uuid.UUID
+    certification: CertificationSchema
+    genres: List[GenreSchema]
+    stars: List[StarSchema]
+    directors: List[DirectorSchema]
 
     model_config = {"from_attributes": True}
