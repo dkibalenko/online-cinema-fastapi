117c67130f8493db6b722985535dbbf3c4c5c5b4
Denys Kibalenko <dennieking87@gmail.com>
2026-01-20 15:52:44 +0100

    Feat: add 'UserGroupEnum, 'GenderEnum', 'UserGroup', 'User' models to src/database/models/accounts.py

---
+++ src/database/models/accounts.py
@@ -0,0 +1,77 @@
+from typing import List
+import enum
+from datetime import datetime, date, timedelta, timezone
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Enum, Integer, String, Boolean, DateTime, func, ForeignKey
+
+from database.models.base import Base
+
+
+class UserGroupEnum(str, enum.Enum):
+    USER = "user"
+    MODERATOR = "moderator"
+    ADMIN = "admin"
+
+
+class GenderEnum(str, enum.Enum):
+    MAN = "man"
+    WOMAN = "woman"
+
+
+class UserGroup(Base):
+    __tablename__ = "user_groups"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    name: Mapped[UserGroupEnum] = mapped_column(
+        Enum(UserGroupEnum),
+        nullable=False,
+        unique=True
+    )
+    users: Mapped[List["User"]] = relationship("User", back_populates="group")
+
+    def __repr__(self) -> str:
+        return f"UserGroup(id={self.id}, name={self.name})"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    email: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False,
+        unique=True
+    )
+    _hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        nullable=False,
+        default=False
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False
+    )
+    group_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("user_groups.id", ondelete="CASCADE"),
+        nullable=False
+    )
+    group: Mapped["UserGroup"] = relationship(
+        "UserGroup",
+        back_populates="users"
+    )
