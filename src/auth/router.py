88cf89e68dbcb2fe6591ad29584046117139ebb1
Denys Kibalenko <dennieking87@gmail.com>
2026-02-01 18:15:25 +0100

    Refactor: update 'POST /register/', 'register_user' endpoint in src/auth/router.py

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,10 +1,11 @@
 from typing import Annotated
+
+from auth.dependencies import get_auth_service
+from auth.service import AuthService
 from fastapi import APIRouter, Depends, status, HTTPException
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import select
 
-from database import get_db
 from auth.models import User, UserGroup, UserGroupEnum
 from auth.schemas import (
     UserRegistrationRequestSchema,
@@ -23,42 +24,7 @@ router = APIRouter()
 )
 async def register_user(
     user_data: UserRegistrationRequestSchema,
-    db: Annotated[AsyncSession, Depends(get_db)],
+    auth: Annotated[AuthService, Depends(get_auth_service)],
 ) -> UserRegistrationResponseSchema:
-    existing_stmt = select(User).where((User.email == user_data.email))
-
-    existing_result = await db.execute(existing_stmt)
-    existing_user = existing_result.scalar_one_or_none()
-
-    if existing_user:
-        raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail=f"A user with this email '{user_data.email}' already exists."
-        )
-
-    stmt = select(UserGroup).where(UserGroup.name == UserGroupEnum.USER)
-
-    result = await db.execute(stmt)
-    user_group = result.scalar_one_or_none()
-    if not user_group:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Default user group not found."
-        )
-
-    try:
-        new_user = User.create(
-            email=str(user_data.email),
-            raw_password=user_data.password,
-            group_id=user_group.id
-        )
-        db.add(new_user)
-        await db.commit()
-    except SQLAlchemyError as e:
-        await db.rollback()
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="An error occurred during user creation."
-        ) from e
-
-    return new_user
+    user = await auth.register_user(user_data)
+    return user
