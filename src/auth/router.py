553a2153ce24867e6938a6aa4fcc91a53e5b7d71
Denys Kibalenko <dennieking87@gmail.com>
2026-02-03 18:23:34 +0100

    Feat: add 'POST /activate/' - 'activate_account' endpoint to src/auth/router.py

--- src/auth/router.py
+++ src/auth/router.py
@@ -2,12 +2,11 @@ from typing import Annotated
 
 from auth.dependencies import get_auth_service
 from auth.service import AuthService
-from fastapi import APIRouter, Depends, status, HTTPException
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy import select
+from fastapi import APIRouter, Depends, status
 
-from auth.models import User, UserGroup, UserGroupEnum
 from auth.schemas import (
+    MessageResponseSchema,
+    UserActivationRequestSchema,
     UserRegistrationRequestSchema,
     UserRegistrationResponseSchema,
 )
@@ -28,3 +27,17 @@ async def register_user(
 ) -> UserRegistrationResponseSchema:
     user = await auth.register_user(user_data)
     return user
+
+
+@router.post(
+    "/activate/",
+    response_model=MessageResponseSchema,
+    summary="User Activation",
+    status_code=status.HTTP_200_OK
+)
+async def activate_account(
+    activatation_data: UserActivationRequestSchema,
+    auth: Annotated[AuthService, Depends(get_auth_service)],
+) -> MessageResponseSchema:
+    message = await auth.activate_account(activatation_data)
+    return message
