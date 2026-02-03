c5291a40ce941522c181b539e29619100616f5f4
Denys Kibalenko <dennieking87@gmail.com>
2026-02-03 18:22:03 +0100

    Feat: add 'AuthService.activate_account' to src/auth/service.py

--- src/auth/service.py
+++ src/auth/service.py
@@ -1,15 +1,21 @@
-from fastapi import APIRouter, Depends, status, HTTPException
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.ext.asyncio import AsyncSession
+from datetime import datetime, timezone
+from typing import cast
 
-from auth.models import ActivationToken, User, RefreshToken
-from auth.utils import hash_password, verify_password
+from fastapi import status, HTTPException
+from sqlalchemy.exc import SQLAlchemyError
+
+from auth.models import ActivationToken, User
 from auth.schemas import (
+    MessageResponseSchema,
     UserRegistrationRequestSchema,
+    UserActivationRequestSchema,
     UserLoginRequestSchema,
 )
 from auth.interfaces import JWTAuthManagerInterface, EmailSenderInterface
 from auth.repository import UserRepository
+from logger_config import get_logger
+
+log = get_logger()
 
 
 class AuthService:
@@ -28,6 +34,22 @@ class AuthService:
         self,
         user_data: UserRegistrationRequestSchema
     ) -> User:
+        """
+        Registers a new user based on the provided registration data.
+
+        Args:
+            user_data (UserRegistrationRequestSchema): The registration data
+
+        Returns:
+            User: The newly created user
+
+        Raises:
+            HTTPException: If a user with the same email already exists
+            HTTPException: If the default user group is not found
+            HTTPException: If an error occurs during user creation
+        """
+        log.info(f"Registration attempt for {user_data.email}")
+
         # 1. Check email uniqueness
         existing_user = await self.users.get_by_email(user_data.email)
 
@@ -60,6 +82,9 @@ class AuthService:
             self.users.add(token)
 
             await self.users.commit()
+            # here, token is set right away before flush() & is available, so,
+            # no need to query again. Also, refresh(user) need not be called,
+            # since atts are valid after commit (expire_on_commit=False)
         except SQLAlchemyError:
             await self.users.rollback()
             raise HTTPException(
@@ -70,7 +95,7 @@ class AuthService:
         # 4. Send activation email
         # activation_link = f"{settings.FRONTEND_URL}/activate?token={token.token}"
         activation_link = (
-            f"http://127.0.0.1:8000/api/v1/accounts/activate?"
+            f"http://127.0.0.1:8000/api/v1/cinema/auth/activate?"
             f"token={token.token}"
         )
         await self.email_sender.send_activation_email(
@@ -78,4 +103,73 @@ class AuthService:
             activation_link=activation_link
         )
 
+        log.info(f"Registration successful for {user_data.email}")
+
         return user
+
+    async def activate_account(
+        self,
+        activation_data: UserActivationRequestSchema,
+    ) -> MessageResponseSchema:
+        """
+        Activate a user account based on the activation token.
+
+        Args:
+            activation_data (UserActivationRequestSchema): The activation data
+                containing the user's email and activation token.
+
+        Returns:
+            MessageResponseSchema: A response containing a success message.
+
+        Raises:
+            HTTPException: If the activation token is invalid or expired, or
+                if the user account is already active.
+        """
+        log.info(f"Activation attempt for {activation_data.email}")
+
+        # 1. Fetch token + user
+        token_record = await self.users.get_activation_token_record(
+            email=activation_data.email,
+            token=activation_data.token
+        )
+
+        now_utc = datetime.now(timezone.utc)
+
+        # 2. Validate token existence and expiration
+        if not token_record or token_record.expires_at < now_utc:
+            if token_record:
+                await self.users.delete_activation_token(token_record)
+                await self.users.commit()
+
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Invalid or expired activation token."
+            )
+
+        user = token_record.user
+
+        # 3. Prevent double activation
+        if user.is_active:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="User account is already active."
+            )
+
+        # 4. Activate user + delete token in one commit
+        user.is_active = True
+        await self.users.delete_activation_token(token_record)
+        await self.users.commit()
+
+        # 5. Send confirmation email
+        login_link = "http://127.0.0.1:8000/api/v1/cinema/auth/login"
+
+        await self.email_sender.send_activation_complete_email(
+            email=str(user.email),
+            login_link=login_link
+        )
+
+        log.info(f"User {user.email} activated successfully")
+
+        return MessageResponseSchema(
+            message="Account activated successfully."
+        )
