0efcf9ffdd1775238f647c7bf95e7a305d900d01
Denys Kibalenko <dennieking87@gmail.com>
2026-02-14 18:54:14 +0100

    Feat: add 'PATCH /me/profile'' - 'update_my_profile' & 'DELETE /me/profile/' - 'delete_my_profile' endpoints to src/users/router.py

--- src/users/router.py
+++ src/users/router.py
@@ -7,14 +7,19 @@ from auth.dependencies import get_current_user
 from users.dependencies import get_users_service, get_token
 from users.service import UserService
 from users.models import User
-from users.schemas import ProfileResponseSchema, ProfileCreationSchema
+from auth.schemas import MessageResponseSchema
+from users.schemas import (
+    ProfileResponseSchema,
+    ProfileCreationSchema,
+    ProfileUpdateSchema
+)
 
 
 router = APIRouter()
 
 
 @router.get(
-    "/me/",
+    "/me/profile/",
     response_model=ProfileResponseSchema,
     summary="Get current user's profile",
     status_code=status.HTTP_200_OK,
@@ -67,3 +72,49 @@ async def create_user_profile(
         avatar=avatar_url,
         user_id=profile.user_id
     )
+
+
+@router.patch(
+    "/me/profile/",
+    response_model=ProfileResponseSchema,
+    summary="Update current user's profile",
+    status_code=status.HTTP_200_OK,
+)
+async def update_my_profile(
+    request: Request,
+    data: Annotated[ProfileUpdateSchema, Depends(ProfileUpdateSchema.as_form)],
+    current_user: Annotated[User, Depends(get_current_user)],
+    users: Annotated[UserService, Depends(get_users_service)],
+    jwt_token: Annotated[str, Depends(get_token)],
+):
+    profile, avatar_url = await users.update_my_profile(
+        current_user.id,
+        data,
+        jwt_token
+    )
+
+    return ProfileResponseSchema(
+        id=profile.id,
+        first_name=profile.first_name,
+        last_name=profile.last_name,
+        gender=profile.gender,
+        date_of_birth=profile.date_of_birth,
+        info=profile.info,
+        avatar=avatar_url,
+        user_id=profile.user_id,
+    )
+
+
+@router.delete(
+    "/me/profile/",
+    response_model=MessageResponseSchema,
+    summary="Delete current user's profile",
+    status_code=status.HTTP_200_OK,
+)
+async def delete_my_profile(
+    current_user: Annotated[User, Depends(get_current_user)],
+    users: Annotated[UserService, Depends(get_users_service)],
+    jwt_token: Annotated[str, Depends(get_token)],
+) -> MessageResponseSchema:
+    await users.delete_my_profile(current_user.id, jwt_token)
+    return MessageResponseSchema(message="Profile deleted.")
