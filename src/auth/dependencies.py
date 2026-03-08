f518a281ca6ebc44461cda7cf7525636eb06e8bb
Denys Kibalenko <dennieking87@gmail.com>
2026-03-08 12:00:48 +0100

    Refactor: update 'get_current_user' dep by preselecting user's group when query 'users' table in src/auth/dependencies.py

--- src/auth/dependencies.py
+++ src/auth/dependencies.py
@@ -2,6 +2,7 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
+from sqlalchemy.orm import selectinload
 
 from config import get_settings, BaseAppSettings
 from database import get_db
@@ -98,7 +99,11 @@ async def get_current_user(
             detail="Invalid token",
         )
 
-    result = await db.execute(select(User).where(User.id == user_id_int))
+    result = await db.execute(
+        select(User)
+        .options(selectinload(User.group))
+        .where(User.id == user_id_int)
+    )
     user = result.scalar_one_or_none()
 
     if not user:
