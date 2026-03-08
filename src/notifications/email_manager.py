ae1655e60733b439192ce23cf14a73822925a13c
Denys Kibalenko <dennieking87@gmail.com>
2026-03-08 12:24:37 +0100

    Chore: format with Ruff src/notifications/email_manager.py

--- src/notifications/email_manager.py
+++ src/notifications/email_manager.py
@@ -1,5 +1,5 @@
-from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 import aiosmtplib
 from jinja2 import Environment, FileSystemLoader, TemplateNotFound
@@ -8,12 +8,10 @@ from logger_config import get_logger
 from notifications.exceptions import BaseEmailError
 from notifications.interfaces import EmailSenderInterface
 
-
 log = get_logger()
 
 
 class EmailSender(EmailSenderInterface):
-
     def __init__(
         self,
         hostname: str,
@@ -56,9 +54,9 @@ class EmailSender(EmailSenderInterface):
         self._validate_template(self._password_complete_email_template_name)
         self._validate_template(self._comment_reply_template_name)
 
-
     @property
     def comment_reply_template(self):
+        """The name of the comment reply email template."""
         return self._comment_reply_template_name
 
     def _validate_template(self, template_name: str | None):
@@ -67,17 +65,14 @@ class EmailSender(EmailSenderInterface):
 
         try:
             self._env.get_template(template_name)
-        except TemplateNotFound:
+        except TemplateNotFound as error:
             raise BaseEmailError(
                 f"Email template '{template_name}' not found in directory "
                 f"'{self._env.loader.searchpath}'."
-            )
+            ) from error
 
     async def _send_email(
-        self,
-        recipient: str,
-        subject: str,
-        html_content: str
+        self, recipient: str, subject: str, html_content: str
     ) -> None:
         message = MIMEMultipart()
         message["From"] = self._from_email
@@ -94,7 +89,7 @@ class EmailSender(EmailSenderInterface):
             smtp = aiosmtplib.SMTP(
                 hostname=self._hostname,  # SMTP_SERVER
                 port=self._port,  # SMTP_PORT
-                start_tls=self._use_tls
+                start_tls=self._use_tls,
             )
 
             await smtp.connect()
@@ -102,7 +97,10 @@ class EmailSender(EmailSenderInterface):
 
             log.debug(f"Authenticating as {self._username}")
             # MailHog(plain SMTP) ignores this, no error
-            await smtp.login(self._username, self._password)  # SMTP_USERNAME, SMTP_PASSWORD
+            await smtp.login(
+                self._username,
+                self._password,  # SMTP_USERNAME, SMTP_PASSWORD
+            )
 
             log.info(f"Sending email to {recipient}")
             await smtp.sendmail(
@@ -114,21 +112,19 @@ class EmailSender(EmailSenderInterface):
         except aiosmtplib.SMTPException as error:
             log.error(f"❌ Failed to send email to {recipient}: {error}")
             raise BaseEmailError(
-                f"❌ Failed to send email to {recipient}: {error}"
-            )
+                f"❌ Failed to send email to {recipient}: {error}",
+            ) from error
 
     async def send_activation_email(
-        self,
-        email: str,
-        activation_link: str
+        self, email: str, activation_link: str
     ) -> None:
-        """
-        Send an account activation email asynchronously.
+        """Send an account activation email asynchronously.
 
         Args:
             email (str): The recipient's email address.
             activation_link (str): The activation link to be included
                 in the email.
+
         """
         log.debug(
             f"Rendering activation template "
@@ -148,16 +144,14 @@ class EmailSender(EmailSenderInterface):
         await self._send_email(email, "Account Activation", html_content)
 
     async def send_activation_complete_email(
-        self,
-        email: str,
-        login_link: str
+        self, email: str, login_link: str
     ) -> None:
-        """
-        Send an account activation completion email asynchronously.
+        """Send an account activation completion email asynchronously.
 
         Args:
             email (str): The recipient's email address.
             login_link (str): The login link to include in the email.
+
         """
         log.debug(
             f"Rendering activation complete template "
@@ -175,22 +169,18 @@ class EmailSender(EmailSenderInterface):
         )
 
         await self._send_email(
-            email,
-            "Account Activated Successfully",
-            html_content
+            email, "Account Activated Successfully", html_content
         )
 
     async def send_password_reset_email(
-        self,
-        email: str,
-        reset_link: str
+        self, email: str, reset_link: str
     ) -> None:
-        """
-        Send a password reset request email asynchronously.
+        """Send a password reset request email asynchronously.
 
         Args:
             email (str): The recipient's email address.
             reset_link (str): The reset link to be included in the email.
+
         """
         log.debug(
             f"Rendering password reset template "
@@ -208,16 +198,14 @@ class EmailSender(EmailSenderInterface):
         await self._send_email(email, "Password Reset Request", html_content)
 
     async def send_password_reset_complete_email(
-        self,
-        email: str,
-        login_link: str
+        self, email: str, login_link: str
     ) -> None:
-        """
-        Send a password reset completion email asynchronously.
+        """Send a password reset completion email asynchronously.
 
         Args:
             email (str): The recipient's email address.
             login_link (str): The login link to be included in the email.
+
         """
         log.debug(
             f"Rendering password reset complete template "
@@ -235,38 +223,28 @@ class EmailSender(EmailSenderInterface):
         )
 
         await self._send_email(
-            email,
-            "Your Password Has Been Successfully Reset",
-            html_content
+            email, "Your Password Has Been Successfully Reset", html_content
         )
 
     async def send_custom_email(
-        self,
-        email: str,
-        subject: str,
-        template_name: str,
-        context: dict
+        self, email: str, subject: str, template_name: str, context: dict
     ) -> None:
-        """
-        Send a custom email asynchronously.
+        """Send a custom email asynchronously.
 
         Args:
             email (str): The recipient's email address.
             subject (str): The email subject.
             template_name (str): The name of the email template.
             context (dict): The context data for the email template.
+
         """
-        log.debug(
-            f"Rendering custom template "
-            f"'{template_name}' for {email}"
-        )
+        log.debug(f"Rendering custom template '{template_name}' for {email}")
 
         template = self._env.get_template(template_name)
         html_content = template.render(**context)
 
         log.debug(
-            f"Rendered custom template "
-            f"({len(html_content)} chars) for {email}"
+            f"Rendered custom template ({len(html_content)} chars) for {email}"
         )
 
         await self._send_email(email, subject, html_content)
