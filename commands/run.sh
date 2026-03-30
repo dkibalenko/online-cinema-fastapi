bb64989cee0da6384f22005db7cde646dcb1027c
Denys Kibalenko <dennieking87@gmail.com>
2026-03-30 16:06:44 +0200

    Refactor: update docker/docker-compose.yml to mount volume to ../src: /usr/src/fastapi/src instead of ../src: /usr/src/fastapi/, so do not override originally created container with the volume

--- commands/run.sh
+++ commands/run.sh
@@ -2,3 +2,4 @@
 
 echo "Starting Uvicorn server..."
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload --reload-dir /usr/src/fastapi
+# uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4  # production load testing
